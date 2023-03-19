53204f2c05a605f0dc220eb584644aa9ad223ed6
abd-wanclouds <abdullah.rasheed@wanclouds.net>
2023-03-20 01:45:12 +0500

    added animation to working progress bar

--- src/components/common/Phase.js
+++ src/components/common/Phase.js
@@ -1,39 +1,64 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import CircularSlider from "@fseehawer/react-circular-slider";
 
 const Phase = ({ value, phase, title, description }) => {
+  const ref = useRef();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async ([entry]) => {
+        await delay(1300);
+        setIsVisible(entry.isIntersecting);
+      },
+      { rootMargin: "-100px" }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, delay]);
+
   return (
-    <div className="relative">
-      <CircularSlider
-        label="."
-        labelColor="#00000000"
-        knobColor="#01c887"
-        progressColorFrom="#A6E1CB"
-        progressColorTo="#00C887"
-        progressSize={16}
-        trackColor="#f2f2f2"
-        trackSize={6}
-        data={[]}
-        dataIndex={value}
-        hideKnob
-        min={0}
-        max={100}
-        width={295}
-        knobDraggable={false}
-        labelFontSize="16px"
-        valueFontSize="24px"
-      />
-      <div className="absolute top-20 left-0 pl-12 md:-mt-4 lg:-mt-2 xl:-mt-2 w-[90%] z-50">
-        <p className="uppercase text-light-green font-bold w-[100%] text-left text-sm">
-          {phase}
-        </p>
-        <p className="uppercase text-light-green text-xl w-[100%] text-left text-base md:text-lg xl:text-xl">
-          {title}
-        </p>
-        <p className="mt-3 text-xs text-white w-[100%] text-left">
-          {description}
-        </p>
-      </div>
+    <div className="relative" ref={ref}>
+      <>
+        <CircularSlider
+          label="."
+          labelColor="#00000000"
+          knobColor="#01c887"
+          progressColorFrom="#A6E1CB"
+          progressColorTo="#00C887"
+          progressSize={16}
+          trackColor="#f2f2f2"
+          trackSize={6}
+          data={[]}
+          dataIndex={isVisible && isVisible ? value : 0}
+          hideKnob
+          min={0}
+          max={100}
+          // width={295}
+          knobDraggable={false}
+          labelFontSize="16px"
+          valueFontSize="24px"
+        />
+        <div className="absolute top-20 left-0 pl-12 md:-mt-4 lg:-mt-2 xl:-mt-2 w-[90%] z-50">
+          <p className="uppercase text-light-green font-bold w-[100%] text-left text-sm">
+            {phase}
+          </p>
+          <p className="uppercase text-light-green text-xl w-[100%] text-left text-base md:text-lg xl:text-xl">
+            {title}
+          </p>
+          <p className="mt-3 text-xs text-white w-[100%] text-left">
+            {description}
+          </p>
+        </div>
+      </>
     </div>
   );
 };
