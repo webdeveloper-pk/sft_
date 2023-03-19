68732115eb8737c949d87c07e34626094ac85436
abd-wanclouds <abdullah.rasheed@wanclouds.net>
2023-03-19 18:51:45 +0500

    fixed chatbot position

--- src/components/SubHero.js
+++ src/components/SubHero.js
@@ -3,8 +3,18 @@ import chat from "../assets/images/chat.png";
 
 const SubHero = () => {
   return (
-    <section class="body-font px-6 md:px-8 py-12 lg:py-16 md:mt-52 homepage-container mx-auto">
-      <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-y-8">
+    <section class="body-font py-12 lg:py-16 md:mt-52">
+      <div className="-mt-4 md:mt-0 w-full md:w-auto relative">
+        <div className="flex flex-row justify-end md:justify-center sticky-custom mr-6">
+          <img
+            src={chat}
+            alt="chat"
+            className="w-[50px] lg:w-[60px]"
+            height="auto"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-y-8 homepage-container mx-auto px-6 md:px-8">
         <div class="flex flex-col items-center md:items-start text-center md:text-left">
           <h1
             class="text-5xl lg:text-6xl xl:text-7xl mb-4 font-medium text-light-black uppercase"
@@ -17,7 +27,7 @@ const SubHero = () => {
             <span className="text-light-green font-bold px-4">FREE!</span>
           </h1>
           <div
-            className="pl-0 md:pl-24 text-light-black text-2xl"
+            className="pl-0 md:pl-20 text-light-black text-2xl -mt-3"
             data-aos="flip-right"
             data-aos-once="true"
             data-aos-duration="3000"
@@ -31,16 +41,6 @@ const SubHero = () => {
               Manage{" "}
               <span className="text-light-green font-bold px-1">100k!</span>
             </p>
-          </div>
-        </div>
-        <div className="-mt-4 md:mt-0 w-full md:w-auto relative pl-[85%] md:pl-[0%] md:pr-12 lg:pr-14">
-          <div className="flex flex-row justify-end md:justify-center sticky-custom">
-            <img
-              src={chat}
-              alt="chat"
-              className="w-[50px] lg:w-[60px]"
-              height="auto"
-            />
           </div>
         </div>
       </div>
