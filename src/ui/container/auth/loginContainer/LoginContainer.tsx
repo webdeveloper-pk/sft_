2761f4a9fde0a76066d94d70f97a4a1ef8a97d40
Hamza-malikx <hayanshyk@gmail.com>
2023-05-30 15:10:18 +0500

    login has been integrated

--- src/ui/container/auth/loginContainer/LoginContainer.tsx
+++ src/ui/container/auth/loginContainer/LoginContainer.tsx
@@ -1,7 +1,48 @@
 import React from "react";
 import Login from "../../../presentation/auth/login/Login";
+import {
+  useAppSelector,
+  useAppDispatch,
+} from "../../../../services/hooks/hooks";
+import {
+  loginUser,
+  resetStatus,
+} from "../../../../store/slices/auth/loginSlice";
+import { useNavigate } from "react-router";
+
 const LoginContainer = () => {
-  return <Login />;
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const loginReducer = useAppSelector((state) => ({
+    status: state.login.status,
+    user: state.login.user,
+    error: state.login.error,
+  }));
+  const [user, setUser] = React.useState({ username: "", password: "" });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const handleLoginUser = React.useCallback(() => {
+    dispatch(loginUser(user));
+  }, [dispatch, user]);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleLoginUser();
+  };
+  React.useEffect(() => {
+    if (loginReducer.status === "succeeded") {
+      // navigate("/dashboard");
+      dispatch(resetStatus());
+    }
+  }, [loginReducer?.status]);
+  return (
+    <Login
+      user={user}
+      loginReducer={loginReducer}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  );
 };
 
 export default LoginContainer;
